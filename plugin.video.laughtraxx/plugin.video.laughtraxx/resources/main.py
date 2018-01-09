1492837abc85fb649b9bbf5e9742cf77b6ce0081
Doggmatic71 <doggmatic71@gmail.com>
2018-01-08 21:43:25 -0500

    Update main.py

--- plugin.video.laughtraxx/plugin.video.laughtraxx/resources/main.py
+++ plugin.video.laughtraxx/plugin.video.laughtraxx/resources/main.py
@@ -581,7 +581,7 @@
                 <tvshowtitle>Sanford & Son</tvshowtitle>
                 <year></year>
         </meta>
-        <link></link>
+        <link>https://raw.githubusercontent.com/Doggmatic71/maddogg/master/plugin.video.laughtraxx/plugin.video.laughtraxx/resources/SCTVseasons.xml</link>
 	    <thumbnail>https://www.thetvdb.com/banners/_cache/posters/186241-2.jpg</thumbnail>
 	    <fanart>https://www.thetvdb.com/banners/_cache/fanart/original/186241-1.jpg</fanart>
 </dir>
